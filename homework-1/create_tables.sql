79a515e14223eef251392828c01cdaf94334fb5b
Екатерина <ekaterina.s3006@yandex.ru>
2023-10-08 19:33:58 +0300

    hw - 1

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,22 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE customers (
+  customer_id varchar (5) PRIMARY KEY,
+  company_name varchar (50),
+  contact_name varchar (50)
+);
+
+CREATE TABLE employees (
+	employee_id smallint PRIMARY KEY,
+	first_name varchar (10),
+	last_name varchar (10),
+	title varchar (50),
+	birth_date date,
+	notes text
+);
+CREATE TABLE orders (
+	order_id integer PRIMARY KEY,
+	customer_id varchar (5) REFERENCES customers(customer_id),
+	employee_id smallint REFERENCES employees(employee_id),
+	order_date date,
+	ship_city varchar (25)
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,68 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+import csv
+def setup_connection():
+    with psycopg2.connect("dbname='north' user='postgres' host='localhost' password='Katerina321'") as north_conn:
+
+        #print(f"Autocommit: {north_conn.autocommit} and Isolation Level: {north_conn.isolation_level}")
+
+        # change the behavior of commit
+        north_conn.autocommit = True
+
+       # print(f"Autocommit: {north_conn.autocommit} and Isolation Level: {north_conn.isolation_level}")
+        return north_conn
+
+def fill_customers_table():
+    north_conn = setup_connection()
+    with north_conn.cursor() as dbtest_curs:
+        with open('north_data/customers_data.csv', newline='', encoding='utf-8') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                cust_id = row['customer_id']
+                comp_name = row['company_name']
+                contact_name = row['contact_name']
+                try:
+                    dbtest_curs.execute(f"INSERT INTO customers  VALUES (%s, %s, %s)", [cust_id, comp_name, contact_name])
+                except (Exception, psycopg2.DatabaseError) as error:
+                    print(error)
+
+# fill_customers_table()
+
+def fill_employees_table():
+    north_conn = setup_connection()
+    with north_conn.cursor() as dbtest_curs:
+        with open('north_data/employees_data.csv', newline='', encoding='utf-8') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                empl_id = int(row['employee_id'])
+                first_name = row['first_name']
+                last_name = row['last_name']
+                title = row['title']
+                birth_date = row['birth_date']
+                notes = row['notes']
+                try:
+                    dbtest_curs.execute(f"INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)", [empl_id, first_name, last_name,
+                                                                                                   title, birth_date, notes])
+                except (Exception, psycopg2.DatabaseError) as error:
+                    print(error)
+
+# fill_employees_table()
+
+
+def fill_orders_table():
+    north_conn = setup_connection()
+    with north_conn.cursor() as dbtest_curs:
+        with open('north_data/orders_data.csv', newline='', encoding='utf-8') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                order_id = row["order_id"]
+                customer_id = row["customer_id"]
+                employee_id = row["employee_id"]
+                order_date = row["order_date"]
+                ship_city = row["ship_city"]
+                try:
+                    dbtest_curs.execute(f"INSERT INTO orders VALUES (%s, %s, %s, %s, %s)", [order_id, customer_id,  employee_id, order_date, ship_city])
+                except (Exception, psycopg2.DatabaseError) as error:
+                    print(error)
+
+# fill_orders_table()
