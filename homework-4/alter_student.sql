2659b66ade19abf502461fde01cee56c1d99c0a9
Екатерина <ekaterina.s3006@yandex.ru>
2023-10-15 12:56:07 +0300

    hw - 4

--- homework-4/alter_student.sql
+++ homework-4/alter_student.sql
@@ -1,19 +1,38 @@
 -- 1. Создать таблицу student с полями student_id serial, first_name varchar, last_name varchar, birthday date, phone varchar
-
+CREATE TABLE student (
+  student_id serial PRIMARY KEY,
+  first_name varchar (20),
+  last_name varchar (20)
+  birthday date
+  phone varchar (20)
+);
 
 -- 2. Добавить в таблицу student колонку middle_name varchar
-
+ALTER TABLE student
+ADD COLUMN middle_name varchar;
 
 -- 3. Удалить колонку middle_name
-
+ALTER TABLE student
+DROP COLUMN middle_name;
 
 -- 4. Переименовать колонку birthday в birth_date
-
+ALTER TABLE student
+RENAME COLUMN birthday TO birth_date;
 
 -- 5. Изменить тип данных колонки phone на varchar(32)
-
+ALTER TABLE student
+ALTER COLUMN phone SET DATA TYPE varchar (32);
 
 -- 6. Вставить три любых записи с автогенерацией идентификатора
+INSERT INTO student (first_name, last_name, birth_date, phone)
+VALUES ('Katerina', 'Semenova', '27.05.1998', '+79825473741');
 
+INSERT INTO student (first_name, last_name, birth_date, phone)
+VALUES ('Daria', 'Ivacheva', '28.04.1998', '+79825473751');
+
+INSERT INTO student (first_name, last_name, birth_date, phone)
+VALUES ('Ksenia', 'Ponasenko', '24.08.1991', '+79658286862');
 
 -- 7. Удалить все данные из таблицы со сбросом идентификатор в исходное состояние
+DELETE FROM student;
+ALTER SEQUENCE student_student_id_seq RESTART WITH 1;
